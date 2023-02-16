c9cf474fbb6c57d8c207376c739e24e952ad1456
mwaraem <mwaraem@gmail.com>
2023-02-16 09:46:23 +0300

    third commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,7 +12,15 @@ const Main = () => {
             setMovies(response.data.results)
         });
     }, []);
-    console.log(movie)
+    //   console.log(movie)
+
+    const truncateString = (str, num) => {
+        if (str?.length > num) {
+            return str.slice(0, num) + '...'
+        } else {
+            return str;
+        }
+    }
 
     return (
         <div className='w-full h-[550px] text-white'>
@@ -26,6 +34,7 @@ const Main = () => {
                         <button className='border text-white border-gray-300 py-2 px-5 ml-4'>Watch Later</button>
                     </div>
                     <p className='text-gray-400 text-sm'>Released: {movie?.release_date}</p>
+                    <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>{truncateString(movie?.overview, 150)}</p>
                 </div>
 
             </div>
